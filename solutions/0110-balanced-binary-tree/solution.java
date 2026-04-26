c68b9a5422c893551ba39f558ee5f938b06136a7
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-04-26 17:34:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.5 MB (77.56%)

---
+++ solutions/0110-balanced-binary-tree/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+
+    private int check(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = check(node.left);
+        if (left == -1) return -1;
+
+        int right = check(node.right);
+        if (right == -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return Math.max(left, right) + 1;
+    }
+}

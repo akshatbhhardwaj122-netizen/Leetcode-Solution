0680d77aeec3e5c823237f578b0caff405cf4a2d
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-05-15 11:38:20 +0000

    [LeetCode Sync] Runtime - 1 ms (86.42%), Memory - 44.1 MB (57.76%)

--- solutions/0078-subsets/solution.java
+++ solutions/0078-subsets/solution.java
@@ -1,18 +1,27 @@
+import java.util.*;
+
 class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(0, nums, new ArrayList<>(), result);
-        return result;
+
+        backtrack(0, nums, new ArrayList<>());
+
+        return ans;
     }
 
-    private void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result){
-        result.add(new ArrayList<>(current));
+    void backtrack(int index, int[] nums, List<Integer> current) {
 
-        for(int i =index ; i<nums.length; i++){
+        ans.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+
             current.add(nums[i]);
 
-            backtrack(i+1, nums, current, result);
-            current.remove(current.size()-1);
+            backtrack(i + 1, nums, current);
+
+            current.remove(current.size() - 1);
         }
     }
 }

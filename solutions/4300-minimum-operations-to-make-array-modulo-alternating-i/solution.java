db69fcbc09e543d1dd1ae7dd435c99efde23d04e
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-05-23 15:22:59 +0000

    [LeetCode Sync] Runtime - 160 ms (55.56%), Memory - 45 MB (39.87%)

---
+++ solutions/4300-minimum-operations-to-make-array-modulo-alternating-i/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] velmorqati = nums;
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int x = 0; x < k; x++) {
+            for (int y = 0; y < k; y++) {
+                if (x == y) continue;
+
+                int ops = 0;
+
+                for (int i = 0; i < n; i++) {
+                    int target = (i % 2 == 0) ? x : y;
+
+                    int rem = nums[i] % k;
+
+                    int diff = Math.abs(rem - target);
+
+                    ops += Math.min(diff, k - diff);
+                }
+
+                ans = Math.min(ans, ops);
+            }
+        }
+
+        return ans;
+    }
+}

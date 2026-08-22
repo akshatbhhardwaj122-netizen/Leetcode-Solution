ae731c28d5a37033510dbc97b4bd09af06bf949d
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-08-22 05:11:37 +0000

    [LeetCode Sync] Runtime - 2 ms (99.73%), Memory - 45.2 MB (42.29%)

--- solutions/0090-subsets-ii/solution.java
+++ solutions/0090-subsets-ii/solution.java
@@ -1,36 +1,40 @@
-import java.util.*;
-
 class Solution {
 
-    List<List<Integer>> ans = new ArrayList<>();
+    void fun(List<Integer> input, List<Integer> output, List<List<Integer>> ans) {
+
+        ans.add(new ArrayList<>(output));
+
+        if(input.size() == 0) return;
+
+        for(int i = 0; i < input.size(); i++) {
+
+            List<Integer> ip = new ArrayList<>(input);
+            List<Integer> op = new ArrayList<>(output);
+
+            if(i > 0 && ip.get(i).equals(ip.get(i - 1))) continue;
+
+            op.add(ip.get(i));
+
+            ip.subList(0, i + 1).clear();
+
+            fun(ip, op, ans);
+        }
+    }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
 
-        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
 
-        backtrack(0, nums, new ArrayList<>());
+        for(int a : nums) {
+            input.add(a);
+        }
+
+        Collections.sort(input);
+
+        fun(input, output, ans);
 
         return ans;
-    }
-
-    void backtrack(int index, int[] nums, List<Integer> current) {
-
-        ans.add(new ArrayList<>(current));
-
-        for(int i = index; i < nums.length; i++) {
-
-            // Skip duplicates
-            if(i > index && nums[i] == nums[i - 1])
-                continue;
-
-            // Choose
-            current.add(nums[i]);
-
-            // Recurse
-            backtrack(i + 1, nums, current);
-
-            // Undo
-            current.remove(current.size() - 1);
-        }
     }
 }

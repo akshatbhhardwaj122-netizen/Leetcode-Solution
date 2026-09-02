e91d658211bee15d90d342da72f2a8860a4598ab
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-09-02 09:33:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (5.83%)

--- solutions/0198-house-robber/solution.java
+++ solutions/0198-house-robber/solution.java
@@ -1,32 +1,20 @@
 class Solution {
-    static int[] dp;
-
+    int []dp;
     public int rob(int[] nums) {
-        int n = nums.length;
-
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-
-        return loot(0, nums);
+        int n=nums.length;
+        dp=new int[n];
+       for (int j = 0; j < n; j++) dp[j] = -1;
+    return fun(nums,0,0);
     }
-
-    public int loot(int i, int[] nums) {
-
-        // Base case
-        if (i >= nums.length)
+    int fun(int nums[],int i,int ans){
+        if(i>=nums.length){
             return 0;
-
-        // Already calculated
-        if (dp[i] != -1)
-            return dp[i];
-
-        // Rob current house
-        int loot = nums[i] + loot(i + 2, nums);
-
-        // Skip current house
-        int skip = loot(i + 1, nums);
-
-        // Store maximum answer
-        return dp[i] = Math.max(skip, loot);
+        } 
+         if (dp[i] != -1) return dp[i];
+        int skip1= nums[i]+fun(nums,i+2,ans);
+        int skip2=fun(nums,i+1,ans);
+        ans = Math.max(skip1,skip2);
+        dp[i]= ans;
+        return ans;
     }
 }

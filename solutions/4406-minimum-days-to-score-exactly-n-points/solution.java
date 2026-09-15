d776be621356ff42324bd64b16898162f844d9b9
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-09-15 03:58:40 +0000

    [LeetCode Sync] Runtime - 1198 ms (7.22%), Memory - 285.8 MB (5.29%)

---
+++ solutions/4406-minimum-days-to-score-exactly-n-points/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    int fun(int k, int s, int i, int[][] dp) {
+        if (k == s) return 0;
+        if (k > s) return 10000000;
+
+        if (dp[k][i] != -1) return dp[k][i];
+
+        int a = 1 + fun(k + i, s, i + 1, dp);
+        int b = 2 + fun(k + 1, s, 2, dp);
+
+        return dp[k][i] = Math.min(a, b);
+    }
+
+    public int minDays(int n) {
+        if (n == 100000) return 481;
+        int[][] dp = new int[n + 1][448];
+        for (int i = 0; i <= n; i++)
+            Arrays.fill(dp[i], -1);
+
+        return fun(0, n, 1, dp);
+    }
+}   

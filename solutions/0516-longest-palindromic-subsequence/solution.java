d40600d20a82e18c0cde5ec8938db80cd72f8ba7
Akshat Bhardwaj <akshatbhhardwaj122@gmail.com>
2026-09-16 04:37:37 +0000

    [LeetCode Sync] Runtime - 28 ms (83.44%), Memory - 64.2 MB (28.48%)

---
+++ solutions/0516-longest-palindromic-subsequence/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    int [][]dp;
+    public int longestPalindromeSubseq(String s) {
+        dp=new int [s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                dp[i][j]=-1;
+            }
+
+        }
+        return fun(s, 0, s.length() - 1,dp);
+    }
+
+    int fun(String s, int i, int j,int[][]dp) {
+        if (i > j) return 0;
+        if (i == j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if (s.charAt(i) == s.charAt(j)) {
+            return dp[i][j]= 2 + fun(s, i + 1, j - 1,dp);
+        }
+        return dp[i][j]=Math.max(fun(s, i + 1, j,dp), fun(s, i, j - 1,dp));
+    }
+}   
